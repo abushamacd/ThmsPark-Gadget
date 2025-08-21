c60bc6748aae28ad6e3f821739c1dde591aaad29
abushamacd <assiddik001@gmail.com>
2025-08-21 17:14:38 +0600

    theme switcher

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,32 @@
 jQuery(document).ready(function () {
   console.log("From main js");
 });
+
+const checkbox = document.getElementById("themeToggle");
+const htmlTag = document.documentElement;
+
+// Load theme from localStorage
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  htmlTag.classList.add("light");
+  htmlTag.classList.remove("dark");
+  checkbox.checked = true;
+} else {
+  htmlTag.classList.add("dark"); // default
+  htmlTag.classList.remove("light");
+  checkbox.checked = false;
+}
+
+// Listen for changes
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    htmlTag.classList.remove("dark");
+    htmlTag.classList.add("light");
+    localStorage.setItem("theme", "light");
+  } else {
+    htmlTag.classList.remove("light");
+    htmlTag.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
