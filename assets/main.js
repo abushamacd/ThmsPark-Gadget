c65b4bc78065a1e54b8756f5bdf1dd75e8ecb707
abushamacd <assiddik001@gmail.com>
2025-09-02 16:35:34 +0600

    upda0te

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,37 @@
 jQuery(document).ready(function () {
-  console.log("From main js");
-});
+  $(document).ready(function () {
+    // Theme Switcher
+    const $checkbox = $("#themeToggle");
+    const $htmlTag = $("html");
 
-const checkbox = document.getElementById("themeToggle");
-const htmlTag = document.documentElement;
+    // Load theme from localStorage
+    const savedTheme = localStorage.getItem("theme");
 
-// Load theme from localStorage
-const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+      $htmlTag.addClass("light").removeClass("dark");
+      $checkbox.prop("checked", true);
+    } else {
+      $htmlTag.addClass("dark").removeClass("light"); // default
+      $checkbox.prop("checked", false);
+    }
 
-if (savedTheme === "light") {
-  htmlTag.classList.add("light");
-  htmlTag.classList.remove("dark");
-  checkbox.checked = true;
-} else {
-  htmlTag.classList.add("dark"); // default
-  htmlTag.classList.remove("light");
-  checkbox.checked = false;
-}
-
-// Listen for changes
-checkbox.addEventListener("change", () => {
-  if (checkbox.checked) {
-    htmlTag.classList.remove("dark");
-    htmlTag.classList.add("light");
-    localStorage.setItem("theme", "light");
-  } else {
-    htmlTag.classList.remove("light");
-    htmlTag.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-  }
+    // Listen for changes
+    $checkbox.on("change", function () {
+      if ($checkbox.is(":checked")) {
+        $htmlTag.removeClass("dark").addClass("light");
+        localStorage.setItem("theme", "light");
+      } else {
+        $htmlTag.removeClass("light").addClass("dark");
+        localStorage.setItem("theme", "dark");
+      }
+    });
+    // Sticy
+    $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 50) {
+        $("#navbar").addClass("sticky_navbar");
+      } else {
+        $("#navbar").removeClass("sticky_navbar");
+      }
+    });
+  });
 });
