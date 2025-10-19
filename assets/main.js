92d820f4edf370f84a278a0fe9ff2a894596402e
abushamacd <assiddik001@gmail.com>
2025-10-19 08:10:17 +0600

    promo sale

--- assets/main.js
+++ assets/main.js
@@ -54,5 +54,36 @@ jQuery(document).ready(function () {
             <path d="M12.2959 0.454104L19.0459 7.2041C19.1508 7.30862 19.234 7.43281 19.2908 7.56956C19.3476 7.7063 19.3768 7.85291 19.3768 8.00098C19.3768 8.14904 19.3476 8.29565 19.2908 8.4324C19.234 8.56915 19.1508 8.69334 19.0459 8.79785L12.2959 15.5479C12.0846 15.7592 11.7979 15.8779 11.4991 15.8779C11.2002 15.8779 10.9135 15.7592 10.7022 15.5479C10.4908 15.3365 10.3721 15.0499 10.3721 14.751C10.3721 14.4521 10.4908 14.1654 10.7022 13.9541L15.5313 9.12504L1.75 9.12504C1.45163 9.12504 1.16548 9.00651 0.954505 8.79554C0.743527 8.58456 0.625 8.29841 0.625 8.00004C0.625 7.70167 0.743527 7.41552 0.954505 7.20455C1.16548 6.99357 1.45163 6.87504 1.75 6.87504L15.5313 6.87504L10.7013 2.04598C10.4899 1.83463 10.3712 1.54799 10.3712 1.2491C10.3712 0.950218 10.4899 0.663574 10.7013 0.45223C10.9126 0.240885 11.1992 0.122151 11.4981 0.122151C11.797 0.122151 12.0837 0.240885 12.295 0.45223L12.2959 0.454104Z" fill="black"/>
             </svg></div>`,
     });
+
+    // count down
+    $(document).ready(function () {
+      // Set your countdown date here (Year, Month-1, Day, Hour, Minute, Second)
+      const countdownDate = new Date("2025-12-31T23:59:59").getTime();
+
+      const timer = setInterval(function () {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        if (distance <= 0) {
+          clearInterval(timer);
+          $("#countdown").hide();
+          $("#finished").removeClass("hidden");
+          return;
+        }
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        $("#days").text(days.toString().padStart(2, "0"));
+        $("#hours").text(hours.toString().padStart(2, "0"));
+        $("#minutes").text(minutes.toString().padStart(2, "0"));
+        $("#seconds").text(seconds.toString().padStart(2, "0"));
+      }, 1000);
+    });
   });
 });
